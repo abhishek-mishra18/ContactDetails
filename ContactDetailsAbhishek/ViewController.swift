874500806ec03bf8ad07bdf9192df90d74c1af28
Abhishek Mishra <abhishek.mishra@zopsmart.com>
2024-07-05 17:03:33 +0530

    Intial Commit

--- ContactDetailsAbhishek/ViewController.swift
+++ ContactDetailsAbhishek/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        image.backgroundColor = .black
+        image.layer.cornerRadius = image.frame.size.height/2
+        image.clipsToBounds = true
+    }
 
 }
 
